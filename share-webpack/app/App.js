7c75c090c071dd488dc0af3c1d9b5a77f8fc4abd
zhouatie <1114349984@qq.com>
2018-02-07 00:19:44 +0800

    finish my blog which about use webpack creat a react-app

--- share-webpack/app/App.js
+++ share-webpack/app/App.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react'
 class App extends Component {
     render() {
         return (
-            <div>hellow I'm component！！！</div>
+            <div>hellow Im component！！！</div>
         )
     }
 }
-
 export default App;
