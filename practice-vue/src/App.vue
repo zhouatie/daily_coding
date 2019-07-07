d5541d9651ba0084011a1cd9d3048b4385cad6e7
zhoust <zhoust@guahao.com>
2019-07-07 21:07:35 +0800

    learn about v-model

---
+++ practice-vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <atie-input v-model="value"></atie-input>{{value}}
+  </div>
+</template>
+
+<script>
+import atieInput from './components/atieInput.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      value: ""
+    }
+  },
+  components: {
+    atieInput
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ practice-vue/src/components/atieInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" :value="inputValue" @input="inputHandler">
+    <img  alt="">
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      inputValue: ''
+    }
+  },
+  props: {
+    value: {
+      default: ''
+    }
+  },
+  methods: {
+    inputHandler(e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
+<style lang="less">
+</style>
