12c515047038965408554499adc2dfe3d4f2f339
zhoust <zhoust@guahao.com>
2019-07-14 23:43:55 +0800

    update practice vue

--- practice-vue/src/App.vue
+++ practice-vue/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
     <atie-input v-model="value"></atie-input>{{value}}
+    <button @click="handleChangeName">name</button>
+    <button @click="handleChangeAge">age</button>
+    <button @click="handleChangeObj">handleChangeObj</button>
+    <span>abc: {{abc}}</span>
+    <div>
+      <!-- 当你想重置input的value的时候，给元素加个key，让vue重新渲染 -->
+      <input v-if="show" type="text" key="1">
+      <input type="text" v-else key="2">
+    </div>
   </div>
 </template>
 
@@ -11,11 +20,45 @@ export default {
   name: 'app',
   data() {
     return {
-      value: ""
+      show: true,
+      value: "",
+      obj: {
+        age: 18,
+        name: 'zhou'
+      },
+      abc: null
     }
   },
   components: {
     atieInput
+  },
+  methods: {
+    handleChangeName() {
+      this.obj.name = `${Date.now()}name`
+    },
+    handleChangeAge() {
+      this.obj.age = `${Date.now()}age`
+    },
+    handleChangeObj() {
+      this.abc = 'zhou'
+    }
+  },
+  mounted() {
+    this.value = 1
+    this.value = 2
+    this.$nextTick(() => {
+      console.log(this.$el)
+    })
+    this.value = 3
+    window._this= this
+  },
+  watch: {
+    'obj.name'(val) {
+      console.log(val, 'watch obj.name')
+    },
+    obj(val) {
+      console.log(val, 'watch obj')
+    }
   }
 }
 </script>
