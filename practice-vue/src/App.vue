4105d57ed1d3c7f47fab121e18cb570c78723d4e
zhoust <zhoust@guahao.com>
2019-07-15 23:29:41 +0800

    update

--- practice-vue/src/App.vue
+++ practice-vue/src/App.vue
@@ -10,29 +10,97 @@
       <input v-if="show" type="text" key="1">
       <input type="text" v-else key="2">
     </div>
+    <select name="" id="" v-model="defaultSelect">
+      <option v-for="list in lists" :value="{name: list.value}" :key="list.id">{{list.value}}</option>
+    </select>
+    {{defaultSelect}}
+    <div>
+      <input type="checkbox" v-model="sex" value="男">
+      <input type="checkbox" v-model="sex" value="女">
+      {{sex}}
+    </div>
+    <div>
+      <input type="text" v-model.number="number" name="" id="">{{typeof number}}{{number}}
+    </div>
+    <div @click="name1 = Date.now()">{{handleName()}}</div>
+    <div class="nav">
+      <abc></abc>
+    </div>
   </div>
 </template>
 
 <script>
 import atieInput from './components/atieInput.vue'
+import abc from './components/menu/menu.vue'
 
 export default {
   name: 'app',
+  components: {
+    abc
+  },
   data() {
     return {
+      navs: [
+        {
+          title: '菜单1'
+        },
+        {
+          title: '菜单2'
+        },
+        {
+          title: '菜单3',
+          children: [
+            {
+              title: '菜单3-1'
+            },
+            {
+              title: '菜单3-2',
+              children: [
+                {
+                  title: '菜单3-2-1'
+                },
+                {
+                  title: '菜单3-2-2'
+                },
+                {
+                  title: '菜单3-2-3'
+                }
+              ]
+            },
+            {
+              title: '菜单3-3'
+            }
+          ]
+        }
+      ],
+      number: 0,
+      sex: [],
       show: true,
       value: "",
       obj: {
         age: 18,
         name: 'zhou'
       },
-      abc: null
+      abc: null,
+      defaultSelect: {name: '菜单2'},
+      lists: [{
+        value: '菜单1',
+        id: 1
+      }, {
+        value: '菜单2',
+        id: 2
+      }],
+      name1: 'zhou',
+      name2: 'atie'
     }
   },
   components: {
     atieInput
   },
   methods: {
+    handleName() {
+      return `${this.name1}${this.name2}`
+    },
     handleChangeName() {
       this.obj.name = `${Date.now()}name`
     },
@@ -42,6 +110,9 @@ export default {
     handleChangeObj() {
       this.abc = 'zhou'
     }
+  },
+  created() {
+    console.log(this.$el, 'el')
   },
   mounted() {
     this.value = 1
